e4380fb94ffff08dc4dfc9a992213cab5e936837
Tiago Godoy <tiago_g_moreira@estudante.sesisenai.org.br>
2022-07-30 01:33:12 -0300

    Remoção de arquivos repetidos

--- DeVinKnowledge/Dica.js
+++
@@ -1,9 +0,0 @@
-export default class Dica{
-    constructor(titulo, linguagem, categoria, descricao, video){
-        this.titulo = titulo
-        this.linguagem = linguagem
-        this.categoria = categoria
-        this.descricao = descricao
-        this.video = video
-    }
-}
